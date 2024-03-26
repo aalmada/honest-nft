668d877305264909533d12d5a3c970314808472b
Antão Almada <antao.almada@gmail.com>
2024-03-26 16:51:11 +0000

    Use CoinMarketCap to get gas usage in Euro

--- packages/blockchain/hardhat.config.ts
+++ packages/blockchain/hardhat.config.ts
@@ -1,10 +1,14 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
 import 'hardhat-gas-reporter';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.24',
 	gasReporter: {
+		coinmarketcap: process.env.COINMARKETCAP_KEY,
 		currency: 'EUR'
 	}
 };
