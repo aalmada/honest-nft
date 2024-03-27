71d1142c3e4791827e638af41e0d27097cee0a5d
Antão Almada <antao.almada@gmail.com>
2024-03-27 11:25:40 +0000

    Add ETHERSCAN_API environment variable

--- packages/blockchain/hardhat.config.ts
+++ packages/blockchain/hardhat.config.ts
@@ -4,11 +4,15 @@ import 'hardhat-gas-reporter';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.local' });
+const { ETHERSCAN_API, COINMARKETCAP_KEY } = process.env;
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.24',
+	etherscan: {
+		apiKey: ETHERSCAN_API
+	},
 	gasReporter: {
-		coinmarketcap: process.env.COINMARKETCAP_KEY,
+		coinmarketcap: COINMARKETCAP_KEY,
 		currency: 'EUR'
 	}
 };
