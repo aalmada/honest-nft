74a669870b8c71c6daea3aed82edbb50d4890280
Antão Almada <antao.almada@gmail.com>
2024-03-28 13:39:56 +0000

    Use Hardhat vars instead of .env

--- packages/blockchain/hardhat.config.ts
+++ packages/blockchain/hardhat.config.ts
@@ -1,18 +1,14 @@
-import { HardhatUserConfig } from 'hardhat/config';
+import { HardhatUserConfig, vars } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
 import 'hardhat-gas-reporter';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.local' });
-const { ETHERSCAN_API, COINMARKETCAP_KEY } = process.env;
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.24',
 	etherscan: {
-		apiKey: ETHERSCAN_API
+		apiKey: vars.get('ETHERSCAN_API')
 	},
 	gasReporter: {
-		coinmarketcap: COINMARKETCAP_KEY,
+		coinmarketcap: vars.get('COINMARKETCAP_KEY'),
 		currency: 'EUR'
 	}
 };
